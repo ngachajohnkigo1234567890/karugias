a05c664c790885a582d4972960cb148fb9e1f5c6
ngachajohnkigo1234567890 <kigojohn347@gmail.com>
2024-07-05 17:33:10 +0300

    login page changes

--- pages/index.php
+++ pages/index.php
@@ -9,18 +9,18 @@
 <body>
     <main class="main">
         <div class="top-bar">
-           <center> <h1>Karugia poultry ranch management system changed code</h1></center>
+           <center> <h1>Karugia poultry ranch management system</h1></center>
         </div>
         <div class="form-div">
             <form action="" method="post" class="LoginForm">
                 <div class="form-control">
-                    <h3>Login</h3>
+                    <h3>Admin Login</h3>
                 </div>
                 <div class="form-control">
                     <input type="text" placeholder="Enter username" class="input">
                 </div>
                 <div class="form-control">
-                    <input type="text" placeholder="Enter password" class="input">
+                    <input type="password" placeholder="Enter password" class="input">
                 </div>
                 <div class="form-control">
                     <button class="btnLogin">Login</button>
