538cfb528f81537aaecf0dc0e5de6e76dbe5862c
ngachajohnkigo1234567890 <kigojohn347@gmail.com>
2024-07-05 17:09:10 +0300

    first commit

---
+++ pages/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+<body>
+    <main class="main">
+        <div class="top-bar">
+           <center> <h1>Karugia poultry ranch management system</h1></center>
+        </div>
+        <div class="form-div">
+            <form action="" method="post" class="LoginForm">
+                <div class="form-control">
+                    <h3>Login</h3>
+                </div>
+                <div class="form-control">
+                    <input type="text" placeholder="Enter username" class="input">
+                </div>
+                <div class="form-control">
+                    <input type="text" placeholder="Enter password" class="input">
+                </div>
+                <div class="form-control">
+                    <button class="btnLogin">Login</button>
+                </div>
+                <div class="form-control">
+                    <p>You do not have an account ? <a href="">Register</a></p>
+                </div>
+            </form>
+        </div>
+    </main>
+</body>
+</html>
